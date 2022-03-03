7ad275f038420d1a53b9b877e1617e83d82cfc80
Tadej Glinšek <tadej.glinsek@gmail.com>
2022-03-03 20:15:12 +0100

    Padalec, nal_4 od zadnjič

--- vaje 2022-02-23/nal_4.m
+++ vaje 2022-02-23/nal_4.m
@@ -1,6 +1,5 @@
 f = @(x, y) sin(x.^2 - y.^4)./(x.^2 - y.^4);
 
-
 % x = linspace(0, 1)
 % y = linspace(0, 2)
 
@@ -28,8 +27,61 @@ N = isnan(G)
 G(N) = 1
 
 surf(A, B, G)
+xlabel('x')
+ylabel('y')
 shading interp
 
 % c
-h = @(x) f(x(1), x(2))  % funkcija, ki jo vnašamo v fmincon, mora imeti le en parameter
-y = fmincon(h, [1/2 1/2], [0 0; 0 0], [0 0], [0 0;0 0], [0 0], [0 0], [1 2])
+F = @(x) f(x(1), x(2))  % funkcija, ki jo vnašamo v fmincon, mora imeti le en parameter
+[x_min, y_min] = fmincon(F, [1/2 1/2], [0 0; 0 0], [0 0], [0 0; 0 0], [0 0], [0 0], [1 2])
+% F(x_min) == y_min
+[x_max, y_max_minus] = fmincon(@(x) -F(x), [1/2 1/2], [0 0; 0 0], [0 0], [0 0; 0 0], [0 0], [0 0], [1 2])
+y_max = -y_max_minus
+% F(x_max) == y_max
+
+hold on
+plot3(x_min(1), x_min(2), y_min, 'o', 'color', 'red')
+plot3(x_max(1), x_max(2), y_max, 'o', 'color', 'red')
+
+% d
+% ni čist tk rešeno, kot je blo mišljeno
+
+% izberi točki x iz [0, 1] in y iz [0, 2]
+x = 3/4
+y = 5/4
+% gradient v točki [x y]
+
+
+x_ = x / h + 1  % lahko se zgodi, da to ne bo celo število
+y_ = y / h + 1
+
+
+% namesto f(A, B) vnesemo kar G (isto, le brez lukenj)
+[FY, FX] = gradient(G)
+% gradient je mal čudna funkcija in je ne razumem čist
+% vrne [FY FX] namest [FX FY], čeprav v dokumentaciji piše drugače
+% lahk da je G narobe definiran
+
+FX = FX./h
+FY = FY./h
+
+%{
+figure
+surf(A, B, G)
+xlabel('x')
+shading interp
+%}
+
+dx = FX(x_, y_)
+dy = FY(x_, y_)
+
+tocka = [x, y, f(x, y)]
+smerni_vektor_normale = [-dx, -dy, 1]./sqrt(dx^2 + dy^2 + 1)
+%{
+quiver3(tocka(1), tocka(2), tocka(3), ...
+    smerni_vektor_normale(1), smerni_vektor_normale(2), smerni_vektor_normale(3), ...
+    'color', 'red')
+%}
+
+
+    
--- vaje 2022-03-02/nal_1.m
+++ vaje 2022-03-02/nal_1.m
@@ -17,14 +17,17 @@ F = @(t, Y) [Y(2) f(Y(2))]';
 % kako drugo cifro
 
 % y je pot, y_ pa hitrost
-y = y_in_y_(:, 1);
+
+% y = y_in_y_(:, 1);
 y_ = y_in_y_(:, 2);
 
+%{
 plot(t, y);
 hold on
 plot(t, y_)
+%}
 povp_hitrost = sum(y_)/length(y_);  % length(y_) je 10000
-fprintf('Povprečna hitrost pride %2.15f\n', povp_hitrost)  % zaokroži na 15 decimalnih mest, + 2 mesti pred piko
+fprintf('Povprečna hitrost pride %.15f m/s.\n', povp_hitrost)  % zaokroži na 15 decimalnih mest, + 2 mesti pred piko
 % pride -37.103943921425703
 
 % 2)
@@ -35,7 +38,7 @@ fprintf('Povprečna hitrost pride %2.15f\n', povp_hitrost)  % zaokroži na 15 de
 % desna stran je negativna, torej bo y' negativen (no, to smo že vedeli)
 % torej y' = -sqrt(g_0/(ro_z * c_u * S/(2*m)))
 y_ = -sqrt(g_0/(ro_z * c_u * S/(2*m)));
-fprintf('Limitna hitrost pride %2.15f\n', y_)
+fprintf('Limitna hitrost pride %.15f m/s.\n', y_)
 % pride -37.435659089009924
 
 % 3)
@@ -48,23 +51,30 @@ F = @(t, Y) [Y(2) f(Y(1), Y(2))]';
 [~, y_in_y_] = ode45(F, t, [40000 0]);
 
 y = y_in_y_(:, 1);
-y_ = y_in_y_(:, 2);
+% y_ = y_in_y_(:, 2);
 
+%{
 figure
 plot(t, y);
 hold on
 plot(t, y_)
+%}
 fprintf("Višina padalca po %d sekundah je %.15d metrov.\n", t_K, y(end))
 % pride 2.892887963680784 * 10^4
 
 % 4)
 visine = [0 : 2000 : 10000, 15000 : 5000 : 30000, 40000];
 gostote = [1.225 1.007 0.8194 0.6601 0.5258 0.4135 0.1948 0.08891 0.04008 0.01841 0.003996];
-n = length(visine);
+
+
 transformacija = @(x) ((x - 40000)./40000).^2;
 visine = transformacija(visine);
-A = [(1 : n)', visine', visine.^2'];
-x = lsqr(A, gostote');  % least squares
+% A = [(1 : length(visine))', visine', visine.^2'];  % alternativna metoda
+% za najmanjše kvadrate, ampak očitno ne dela čist prav. Bolje: polyfit
+% x = lsqr(A, gostote');  % least squares
+
+x = flip(polyfit(visine, gostote, 2))';  % boljši least squares
+
 % naš polinom:
 ro_z = @(y) x(1) + x(2).*transformacija(y) + x(3).*transformacija(y).^2;
 
@@ -74,11 +84,48 @@ f = @(y, y_) nekonkreten_f(g(y), ro_z(y), c_u, S, m, y_);
 F = @(t, Y) [Y(2) f(Y(1), Y(2))]';
 [~, y_in_y_] = ode45(F, t, [40000 0]);
 y = y_in_y_(:, 1);
-y_ = y_in_y_(:, 2);
+% y_ = y_in_y_(:, 2);
 
+%{
 figure
 plot(t, y);
 hold on
 plot(t, y_)
+%}
 fprintf("Višina padalca po %d sekundah je %.15d metrov.\n", t_K, y(end))
-% pride napačnih 3.883342248396916e+03. Prav naj bi blo 4.226372472678798e+03
+% pride             4.226372472678795e+03
+% reštev iz vaj je  4.226372472678798e+03
+
+% 5
+t_K = 30;
+t = linspace(0, t_K, 10000);
+
+[~, y_in_y_] = ode45(F, t, [40000 -3]);  % začetna hitrost 3 m/s navzdol
+% y = y_in_y_(:, 1);
+y_ = y_in_y_(:, 2);
+
+hitrost_z_odrivom = y_(end);
+
+[~, y_in_y_] = ode45(F, t, [40000 0]);
+% y = y_in_y_(:, 1);
+y_ = y_in_y_(:, 2);
+
+fprintf("Razlika hitrosti padalca po %d sekundah glede na skok brez odriva je %.15f m/s.\n", t_K, hitrost_z_odrivom - y_(end))
+% pride -2.251586442889391 m/s
+
+% 6
+
+
+fun2 = @(x) (fun1(x, F) + 300);  % iščemo hitrost -300
+T = fzero(fun2, 30);
+fprintf("Hitrost 300 m/s padalec doseže po %.14f sekundah.\n", T)
+
+function v = fun1(t_K, F)  % hitrost ob t_K
+    t = linspace(0, t_K, 10000);
+    [~, y_in_y_] = ode45(F, t, [40000 0]);
+    % y = y_in_y_(:, 1);
+    y_ = y_in_y_(:, 2);
+    v = y_(end);
+end
+% pride             34.81280761424303
+% rešitev iz vaj je 34.81280761423394
